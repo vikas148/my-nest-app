6c3f6e2ece60e0784c347b9872b75f5b1b902db9
vikas148 <vb334531@gmail.com>
2023-03-04 22:28:03 +0530

    root module

--- src/root.module.ts
+++ src/root.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BookController } from './app.controller';
+import { BookService } from './book.service';
 
 
 @Module({
   imports: [ ],
   controllers: [BookController],
-  providers: [],
+  providers: [BookService],
   exports: [],
 })
 export class RootModule {
